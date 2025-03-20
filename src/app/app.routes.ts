1855d7822e03fc47eac8c0476635c233c837f7be
Mohamed Nazeer <mohamed55nazeer55@gmail.com>
2025-03-20 11:26:32 +0200

    feat: add navbar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { AuthComponent } from './core/layout/auth/auth.component';
-import { UserComponent } from './core/layout/user/user.component';
+import { AuthComponent } from './core/layout/auth-layout/auth.component';
+import { UserComponent } from './core/layout/user-layout/user.component';
 import { LoginComponent } from './core/auth/components/login/login.component';
 import { RegisterComponent } from './core/auth/components/register/register.component';
 import { HomeComponent } from './features/home/components/home/home.component';
@@ -9,17 +9,24 @@ import { CartComponent } from './features/cart/components/cart/cart.component';
 import { NotFoundComponent } from './core/auth/components/not-found/not-found.component';
 
 export const routes: Routes = [
-    {path:'auth', component: AuthComponent, children: [
-        {path:'', redirectTo:'login', pathMatch:'full'},
-        {path: 'login', component: LoginComponent},
-        {path: 'register', component: RegisterComponent}
-    ]},
-    {path: 'user', component: UserComponent, children: [
-        {path:'', redirectTo:'home',pathMatch:'full'},
-        {path:'home', component: HomeComponent},
-        {path:'category', component: CategoryComponent},
-        {path:'cart',component: CartComponent}
-    ]},
-    {path:'**', component: NotFoundComponent}
-
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ],
+  },
+  {
+    path: '',
+    component: UserComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'category', component: CategoryComponent },
+      { path: 'cart', component: CartComponent },
+    ],
+  },
+  { path: '**', component: NotFoundComponent },
 ];
