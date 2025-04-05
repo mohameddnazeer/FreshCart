a89f725e790e16682c94d051448284a664e1ee89
nazeeer <mohamed55nazeer55@gmail.com>
2025-04-05 17:11:15 +0200

    feat: add validation reusable component and use in register page

--- src/app/core/auth/components/register/register.component.ts
+++ src/app/core/auth/components/register/register.component.ts
@@ -1,20 +1,40 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ValidationMessagesComponent } from "../../../../shared/components/validation-messages/validation-messages.component";
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, ValidationMessagesComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  // form control
   name = new FormControl(null, [
     Validators.required,
     Validators.minLength(3),
     Validators.maxLength(20)
   ]);
 
+  // form group
+  authForm = new FormGroup({
+    name: this.name,
+    email: new FormControl(null, [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(20),
+      Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,20}$/)
+    ]),
+    rePassword: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(20)
+    ]),
+  })
   submitInput(){
-    console.log(this.name.value);
-    
+    console.log(this.authForm);
   }
 }
